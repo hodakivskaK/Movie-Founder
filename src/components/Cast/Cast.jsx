2d209c230f9328fe459f10f65e1330dbad65d707
Chris <96114298+hodakivskaK@users.noreply.github.com>
2023-11-13 12:41:54 +0200

    fix cast picutre

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -44,7 +44,7 @@ export default function Cast()  {
               src={`https://image.tmdb.org/t/p/w300${castItem.profile_path}`}
                 alt={`${castItem.name} portrait`}
                 className={s.movieCastImg}
-               
+          
               /> 
               }
            </div>
